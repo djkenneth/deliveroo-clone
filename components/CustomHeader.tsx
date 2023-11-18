fa29ddb61f4fcea2c623dd1e4003a6f7f1e064c9
Kenneth Ivan Pineda <dj.kenneth.pineda@gmail.com>
2023-11-18 14:04:18 +0800

    Add vscode formatter

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -25,31 +25,31 @@ const SearchBar = () => (
 )
 
 const CustomHeader = () => {
-    const bottomSheetRef  = useRef<BottomSheetModal>(null)
+    const bottomSheetRef = useRef<BottomSheetModal>(null)
     const openModal = () => {
         bottomSheetRef.current?.present();
     }
-  return (
-    <SafeAreaView style={styles.safeArea}>
-        <BottomSheet ref={bottomSheetRef} />
-        <View style={styles.container}>
-            <TouchableOpacity onPress={openModal}>
-                <Image style={styles.bike} source={require('@/assets/images/bike.png')}/>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
-                <Text style={styles.title}>Delivery Now</Text>
-                <View style={styles.locationName}>
-                    <Text style={styles.subTitle}>London</Text>
-                    <Ionicons name='chevron-down-outline' size={20} color={Colors.primary} />
-                </View>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.profileButton}>
-                <Ionicons name='person-outline' size={20} color={Colors.primary} />
-            </TouchableOpacity>
-        </View>
-        <SearchBar />
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <BottomSheet ref={bottomSheetRef} />
+            <View style={styles.container}>
+                <TouchableOpacity onPress={openModal}>
+                    <Image style={styles.bike} source={require('@/assets/images/bike.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
+                    <Text style={styles.title}>Delivery Now</Text>
+                    <View style={styles.locationName}>
+                        <Text style={styles.subTitle}>London</Text>
+                        <Ionicons name='chevron-down-outline' size={20} color={Colors.primary} />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.profileButton}>
+                    <Ionicons name='person-outline' size={20} color={Colors.primary} />
+                </TouchableOpacity>
+            </View>
+            <SearchBar />
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -81,9 +81,9 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold'
     },
-    locationName: { 
-        flexDirection: 'row', 
-        alignItems: 'center', 
+    locationName: {
+        flexDirection: 'row',
+        alignItems: 'center',
     },
     profileButton: {
         backgroundColor: Colors.lightGray,
